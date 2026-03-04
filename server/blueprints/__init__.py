f29e4e78b146fab081b88529ab6e4efa99293f0a
Mahboob Attar <134129628+Mahboob-Attar@users.noreply.github.com>
2026-03-04 22:20:54 +0000

    fix blueprint imports for railway

--- server/blueprints/__init__.py
+++ server/blueprints/__init__.py
@@ -1,13 +1,14 @@
-from server.blueprints.services.home.routes import home
-from server.blueprints.services.doctors.routes import doctors
-from server.blueprints.services.appointments.routes import appointments
-from server.blueprints.services.feedback.routes import feedback
-from server.blueprints.services.aiml.diagnostic.routes import diagnostic
-from server.blueprints.services.aiml.chatbot.routes import chatbot
-from server.blueprints.services.admin.routes import admin
-from server.blueprints.services.dashboard.routes import dashboard
-from server.blueprints.services.auth.routes import auth
-from server.blueprints.services.availability.routes import availability
+from blueprints.services.home.routes import home
+from blueprints.services.doctors.routes import doctors
+from blueprints.services.appointments.routes import appointments
+from blueprints.services.feedback.routes import feedback
+from blueprints.services.aiml.diagnostic.routes import diagnostic
+from blueprints.services.aiml.chatbot.routes import chatbot
+from blueprints.services.admin.routes import admin
+from blueprints.services.dashboard.routes import dashboard
+from blueprints.services.auth.routes import auth
+from blueprints.services.availability.routes import availability
+
 
 def init_blueprints(app):
     app.register_blueprint(home)
@@ -20,5 +21,3 @@ def init_blueprints(app):
     app.register_blueprint(auth)
     app.register_blueprint(dashboard)
     app.register_blueprint(availability)
-
-
